b64055089f0efc5100b336d9ce0ffd6231a0bcc4
PhethoSilas <mokayps@gmail.com>
2021-06-23 19:13:30 +0200

    I updated the rest api code.

--- Rest_API.py
+++ Rest_API.py
@@ -1,47 +1,43 @@
-
 # importing Flask and other modules
 from flask import Flask, request, jsonify
 import json
-
-# import the connect library from psycopg2
+# importing from the databaseConnection script and modules
+from databaseConnection import *  
 
 app = Flask(__name__)
 
 
-@app.route('/',methods=["GET"])
+# get method to return user's infomation
+@app.route('/', methods=["GET"])
 def test():
-	x = {
-	"name": "John",
-	"age": 30,
-	"city": "New York"
-	}
+    x = {
+        "Firstname": "xxxx",
+        "Surname": "xxxx",
+        "Date of birth": "xxxxx"
+    }
 
-	# convert into JSON:
-	response = json.dumps(x)
-	return response
+    # convert into JSON:
+    response = json.dumps(x)
+    return response
 
-@app.route('/test',methods=["GET"])
-def test2():
-	x = {
-	"Message": "YAY",
-	}
 
-	# convert into JSON:
-	response = json.dumps(x)
-	return response
-
-@app.route('/pheto',methods=["POST"])
+# Posting user infor to the server/database
+@app.route('/info', methods=["POST"])
 def getvalues():
-	data = request.json
+    data = request.json
 
-	print(data)
-	print(data["FirstName"])
-	return jsonify(data)
-	
+    firstname = data["Firstname"]
+    surname = data["Surname"]
+    dateofbirth = data["Date of birth"]
+    # inserting the first name,surname and date of birth into the database
+    cursor.execute('INSERT INTO Userinfo (FIRST_NAME,SURNAME,Date_of_birth) values(%s,%s,%s)',
+                   (
+                       firstname, surname, dateofbirth
+                   ))
+    conn.commit()
+    print("success")
+    return jsonify(data)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
-
-
-# add
-# Add
--- databaseConnection.py
+++ databaseConnection.py
@@ -1,23 +1,25 @@
+# import the connect library from psycopg2
 from psycopg2 import connect
 
 table_name = "some_table"
 
-	# declare connection instance
+# declare connection instance
 conn = connect(
-dbname = "postgres",
-user = "postgres",
-host = "172.28.1.4",
-password = "1234"
-	)
+    dbname="pythondb",
+    user="phetho",
+    password="Password1",
+    port='5432'
+)
 
-	# declare a cursor object from the connection
+# declare a cursor object from the connection
 cursor = conn.cursor()
-cursor .execute("""CREATE TABLE IF NOT EXISTS User_info
-                                                      (
-id INT PRIMARY KEY NOT NULL,
-FirstName INT NOT NULL,
-Surname TEXT NOT NULL,
-Dateofbirth BOOL NOT NULL
-                                                      )"""	)
-# execute an SQL statement using the psycopg2 cursor object
-# cursor.execute(f"SELECT * FROM {table_name};")
+cursor.execute("drop table Userinfo")
+cursor.execute("""CREATE TABLE IF NOT EXISTS Userinfo(
+                      FIRST_NAME varchar(20),
+                      SURNAME varchar(20),
+                      Date_of_birth date
+                       )""")
+
+
+conn.commit()
+
